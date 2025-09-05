427570cf32eb8529a433708d8e962372a24ca7e7
Carlos Alonso <k.alonzo268@icloud.com>
2025-09-05 09:50:19 -0600

    Update Open.py

--- Open.py
+++ Open.py
@@ -4,9 +4,12 @@ red_pallet = hight_intensity_energy
 low_move = low_intensity_energy
 
 # Values for the variables in the bright future martix 💃
-Intensity = 10
-Color = Purple
+Intensity = 10 # Top power
+Color = Purple # High Color
+Strength = Useful # Mine
+Classification = **** # Special
+  
 # A bright future matrix 🧜🏻‍♂️
 Energy = [Intensity, Color, Strength, Classification]
 
-Nucleus = Energy;
+Nucleus = Energy
