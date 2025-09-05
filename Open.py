7a60ebbf21d88815408f6229539801e0b8b7396d
Carlos Alonso <k.alonzo268@icloud.com>
2025-09-05 09:44:18 -0600

    Update Open.py

--- Open.py
+++ Open.py
@@ -3,6 +3,9 @@ blue_jeans = intermediate_power_energy
 red_pallet = hight_intensity_energy
 low_move = low_intensity_energy
 
+# Values for the variables in the bright future martix 💃
+Intensity = 10
+Color = Purple
 # A bright future matrix 🧜🏻‍♂️
 Energy = [Intensity, Color, Strength, Classification]
 
